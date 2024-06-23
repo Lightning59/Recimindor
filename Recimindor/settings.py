27530777a684c19164bcdcc4beb27302891070c5
Evan McCaleb <evanmccaleb@gmail.com>
2024-06-23 19:41:42 -0400

    Setup Postgres Database.

--- Recimindor/settings.py
+++ Recimindor/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'Recimindor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Recimindor',
+        'USER': 'Recimindor',
+        'PASSWORD': 'Recimindor',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
