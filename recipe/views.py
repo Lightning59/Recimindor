449e6c51c56d7a2c305a58a308967a16cebe9c1b
Evan McCaleb <evanmccaleb@gmail.com>
2024-07-04 19:03:40 -0400

    App now lists recipes and descriptions

--- recipe/views.py
+++ recipe/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from recipe.models import Recipe
+
+
 # Create your views here.
 def recipes(request):
-    return render(request, 'recipe/recipes.html')
+    recipes_objs = Recipe.objects.all()
+    context = {'recipes': recipes_objs}
+    return render(request, 'recipe/recipes.html', context)
